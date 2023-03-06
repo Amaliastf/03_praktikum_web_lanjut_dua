7d08f19d592910ef7789a2ad8ccbf7ec019a6f80
Amaliastf <amaliasalutfi@gmail.com>
2023-03-06 19:30:15 +0700

    sembarang

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+        //Prak 1.1 (home)
+        public function page(){
+            return view('page');;
+        }
+
+        
+}
